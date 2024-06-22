77e64dba58f044ca787f068f62ccd128ac8903d0
lyouoda <138292004+lyouoda@users.noreply.github.com>
2024-06-22 13:45:17 +0900

    Use Grid and put three buttons

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <gtk/gtk.h>
 
 static void
-print_hello(GtkWidget *widget,
-            gpointer data)
+button_clicked1(GtkWidget *widget,
+                gpointer data)
 {
-    g_print("Hello, World!\n");
+    g_print("Button1 Clicked\n");
+}
+
+static void
+button_clicked2(GtkWidget *widget,
+                gpointer data)
+{
+    g_print("Button2 Clicked\n");
 }
 
 static void
@@ -13,24 +20,29 @@ activate(GtkApplication *app,
 {
     GtkWidget *window;
     GtkWidget *button;
-    GtkWidget *box;
+    GtkWidget *grid;
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Window");
-    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
-    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-    gtk_widget_set_halign(box, GTK_ALIGN_CENTER);
-    gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
+    grid = gtk_grid_new();
 
-    gtk_window_set_child(GTK_WINDOW (window), box);
+    gtk_window_set_child(GTK_WINDOW(window), grid);
 
-    button = gtk_button_new_with_label("Hello, World!");
+    button = gtk_button_new_with_label("Button1");
+    g_signal_connect(button, "clicked", G_CALLBACK(button_clicked1), NULL);
 
-    g_signal_connect(button, "clicked", G_CALLBACK (print_hello), NULL);
+    gtk_grid_attach(GTK_GRID(grid), button, 0, 0, 1, 1);
+
+    button = gtk_button_new_with_label("Button 2");
+    g_signal_connect(button, "clicked", G_CALLBACK(button_clicked2), NULL);
+
+    gtk_grid_attach(GTK_GRID(grid), button, 1, 0, 1, 1);
+
+    button = gtk_button_new_with_label("Quit");
     g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_window_destroy), window);
 
-    gtk_box_append(GTK_BOX(box), button);
+    gtk_grid_attach(GTK_GRID(grid), button, 0, 1, 2, 1);
 
     gtk_window_present(GTK_WINDOW(window));
 }
